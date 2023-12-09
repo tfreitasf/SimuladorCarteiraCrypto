579b5bd344837468eeb6a5592df4780531aed80e
tfreitasf <tfreitasf@gmail.com>
2023-12-09 12:45:21 -0300

    feat: suporte a imagens SVG no carregamento com Coil
    - Adicionada a dependência coil-svg para permitir o carregamento de imagens SVG.
    - Modificado ImageView.TryLoadImage para usar ImageLoader com SvgDecoder.
      - Agora, além de carregar imagens em formatos comuns, também suporta SVG.
      - Adicionados componentes no ImageLoader para incluir SvgDecoder.Factory.
      - Atualizado o método de carregamento para construir e enfileirar uma ImageRequest.

--- app/src/main/java/br/com/povengenharia/simuladorcarteiracrypto/extensions/ImageViewExtensions.kt
+++ app/src/main/java/br/com/povengenharia/simuladorcarteiracrypto/extensions/ImageViewExtensions.kt
@@ -2,15 +2,26 @@ package br.com.povengenharia.simuladorcarteiracrypto.extensions
 
 import android.widget.ImageView
 import br.com.povengenharia.simuladorcarteiracrypto.R
-import coil.load
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
 
 
 fun ImageView.TryLoadImage(url: String? = null) {
-    load(url) {
-        crossfade(true)
-        fallback(R.drawable.imgfallback)
-        error(R.drawable.errorimg)
-        placeholder(R.drawable.placeholder)
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            add(SvgDecoder.Factory())
+        }
+        .build()
 
-    }
+    val request = ImageRequest.Builder(context)
+        .data(url)
+        .crossfade(true)
+        .fallback(R.drawable.imgfallback)
+        .error(R.drawable.errorimg)
+        .placeholder(R.drawable.placeholder)
+        .target(this)
+        .build()
+    imageLoader.enqueue(request)
+
 }
